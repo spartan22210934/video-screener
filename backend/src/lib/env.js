a3474e13f353ed0cb45088f3cd6ba4e41e56f558
spartan22210934 <parbatkrushnav04@gmail.com>
2025-11-02 12:58:46 +0530

    deployment

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,6 +5,7 @@ export const ENV = {
 
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV,
     
 
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,34 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-
+import path from "path";
 
 
 const app = express();
 const PORT = ENV.PORT || 3000;
-//app.use(express.json());
+
+
+const __dirname = path.resolve();
+
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "success from api" });
 });
+app.get("/books", (req, res) => {
+  res.status(200).json({ status: "api is up and running " });
+}
+);
+
+
+//make ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));     
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+} 
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
